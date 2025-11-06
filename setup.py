233b071ba5dad5fbcbeebb74438a3aa82eefd9ae
Manish <mky80094@gmail.com>
2025-11-07 01:14:47 +0530

    medical chatbott runing fine

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='GenAI-MedicalChatbot-Project',
+    version='0.1.0',
+    author="manish",
+    author_email="mky80094@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
